6e1a136c7ed8412fb3cf93eb77fe40b9f3c992c4
TaYYKaYY <107534876+TaYYKaYY@users.noreply.github.com>
2023-03-16 23:32:18 +0200

    all changes ready to go live

---
+++ app.js
@@ -0,0 +1,109 @@
+let mainDescription = document.querySelector('.desc')
+
+let slideBtn = document.querySelector('.btn-self__slide')
+
+let projectsSection = document.querySelector('#projects')
+
+let aboutSection = document.querySelector('#about')
+
+let aboutInfo = document.querySelector('.about__info')
+
+let aboutSkillsInfo = document.querySelectorAll('.skill-item')
+
+let specialNumbers = document.querySelectorAll('.special-number')
+
+let imageContainers = document.querySelectorAll('.image-container')
+
+let dropDown = document.querySelector('.dropdown')
+
+let age = document.querySelector('.age')
+
+let navList = document.querySelector('.nav-list')
+
+let navbar = document.querySelector('.navbar')
+
+let iconSelf = document.querySelectorAll('.icon-self')
+
+let contact = document.querySelector('.contact')
+
+const firstName = "Tony Khoury"
+
+const profesion = "Front-End Developer"
+
+let index = 1
+let secondIndex = 1
+let date = new Date()
+
+age.textContent = `Age: ${date.getFullYear() - 2002}`
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 200){
+        navbar.classList.add('stick')
+    }else {
+        navbar.classList.remove('stick') 
+    }
+})
+
+let nameInterval = setInterval(() => {
+    mainDescription.children[0].textContent += firstName[index]
+    index++
+    if (index === firstName.length){
+        clearInterval(nameInterval)
+    }
+}, 70)
+let secondInterval = setInterval(() => {
+    mainDescription.children[1].textContent += profesion[secondIndex]
+    secondIndex++
+    if (secondIndex === profesion.length){
+        clearInterval(secondInterval)
+    }
+}, 100)
+
+
+navList.addEventListener('click', e => {
+    if (e.target.getAttribute('data-text').toLowerCase() === 'projects'){
+        projectsSection.scrollIntoView()
+    } else if (e.target.getAttribute('data-text').toLowerCase() === 'about'){
+        aboutSection.scrollIntoView()
+    } else if (e.target.getAttribute('data-text').toLowerCase() === 'home'){
+        window.scrollTo(0, 0)
+    }else if (e.target.getAttribute('data-text').toLowerCase() === 'contact'){
+        contact.classList.toggle('contact-rise')
+    }
+})
+slideBtn.addEventListener('click', () => {
+    projectsSection.scrollIntoView()
+})
+let observer = new IntersectionObserver(enteries => {
+    enteries.forEach(entry => {
+        if (entry.target.classList.contains('image-container')){
+            if (entry.isIntersecting){
+                entry.target.classList.add('rise')
+            }
+        }if (entry.target.classList.contains('special-number')){
+            if (entry.isIntersecting){
+                entry.target.classList.add('rise-number')
+            }
+        }if (entry.target.classList.contains('slider')){
+            if (entry.isIntersecting){
+                entry.target.classList.add('slide')
+            }
+        }if (entry.target.classList.contains('skill-item')){
+            if (entry.isIntersecting){
+                entry.target.classList.add('rise')
+            }
+        }if (entry.target.classList.contains('icon-self')){
+            if (entry.isIntersecting){
+                entry.target.classList.add('rise')
+            }
+        }
+    })
+})
+
+iconSelf.forEach(icon => observer.observe(icon))
+aboutSkillsInfo.forEach(item => observer.observe(item))
+specialNumbers.forEach(number => observer.observe(number))
+imageContainers.forEach(container => observer.observe(container))
+for (let item in aboutInfo.children){
+    observer.observe(aboutInfo.children[item])
+}
